3a76b75e5f987c8356ea00ff4507e3c2fa5835e3
Shehtab Zaman <szaman5@binghamton.edu>
2020-09-01 18:11:52 -0400

    Synthetic data update

--- applications/FAST/data/synth_data.py
+++ applications/FAST/data/synth_data.py
@@ -11,4 +11,28 @@ class Synthetic_Data:
         return self.num_samples 
     
     def __getitem__(self, idx):
-        return np.flatten(np.float32(self.data[idx]))        
+
+        return np.concatenate((np.flatten(np.float32(self.data[idx])),
+        		np.float32(np.random.normal())), axis=None)        
+
+
+class Synthetic_Graph_Data(object):
+	"""docstring for Synthetic_Graph_Data"""
+	def __init__(self, num_samples = 100 , num_nodes = 100):
+		super(Synthetic_Graph_Data, self).__init__()
+		self.num_samples = num_samples
+		self.num_nodes = 100
+		self.adj_matrices = []
+		self.covalent_nodes = []
+		self.non_covalent_nodes = []
+
+	def __len__(self):
+		return self.num_samples
+
+	def __getitem__(self, idx):
+		data = np.concatenate((self.adj_matrices[idx].flatten(),
+							   self.node_features[idx].flatten(),
+							   np.float32(np.random.normal())), 
+							  axis=None)
+		return data
+		
