9da8b321c014e0102ec449cda1e2f4b884192374
Shehtab Zaman <szaman5@binghamton.edu>
2021-03-11 01:41:29 -0500

    - Added synthetic dataset generator - Updated NNConv sparse implementation to highlight message and aggregate decomposition

--- python/lbann/modules/graph/sparse/NNConv.py
+++ python/lbann/modules/graph/sparse/NNConv.py
@@ -13,6 +13,7 @@ class NNConv(Module):
     def __init__(self,
                  sequential_nn,
                  num_nodes,
+                 num_edges,
                  output_channels,
                  activation=lbann.Relu,
                  name=None):
@@ -32,7 +33,9 @@ class NNConv(Module):
                                                   modules for updating the
                                                   edge feature matrix
             num_nodes (int): Number of vertices of each graph
-                            (max number in the batch)
+                            (max number in the batch padded by 0)
+            num_edges (int): Number of edges of each graph
+                            (max in the batch padded by 0)
             output_channels (int): The output size of each node feature after
                                 transformed with learnable weights
             activation (type): The activation function of the node features
@@ -47,6 +50,7 @@ class NNConv(Module):
         self.output_channels = output_channels
 
         self.num_nodes = num_nodes
+        self.num_edges = num_edges
 
         self.node_activation = activation
 
@@ -57,13 +61,77 @@ class NNConv(Module):
                                             name=self.name+"_node_weights")
         self.edge_nn = sequential_nn
 
-    def forward(self, node_features, edge_features, edge_index):
+    def message(self,
+                node_features,
+                neighbor_features,
+                edge_features):
+        """Update node features and edge features. The Message stage of the
+           convolution.
+
+        Args:
+            node_features (Layer); A 2D layer of node features of
+                                   shape (num_nodes, input_channels)
+            neighbor_features (Layer): A 3D layer of node features of
+                                       shape (num_edges, 1, input_channels)
+            edge_features (Layer): A 2D layer of edge features of
+                                   shape (num_edges, edge_features)
+        Returns:
+            (Layer, Layer): Returns the updated node features and the messages
+            for each node.
+        """
+
+        updated_node_features = self.node_nn(node_features)
+
+        edge_update = self.edge_nn(edge_features)
+
+        edge_values = \
+            lbann.Reshape(edge_update,
+                          dims=str_list([self.num_edges,
+                                         self.input_channels,
+                                         self.output_channels]),
+                          name=self.name+"_edge_mat_reshape")
+        edge_values = \
+            lbann.MatMul(neighbor_features, edge_values)
+        return updated_node_features, edge_values
+
+    def aggregate(self,
+                  edge_values,
+                  edge_indices):
+        """Aggregate the messages from the neighbors of the nodes
+
+        Args:
+            edge_values (Layer): A 2D layer of edge features of
+                                   shape (num_edges, edge_features)
+            edge_indices (Layer): A 1D layer of node features of
+                                shape (num_edges * output_channels).
+                                The indices used for reduction
+        Returns:
+            (Layer): A 2D layer of updated node features
+        """
+
+        node_feature_size = self.num_nodes * self.output_channels
+
+        edge_values = lbann.Reshape(edge_values,
+                                    dims=str_list([node_feature_size]))
+        edge_reduce = lbann.Scatter(edge_values,
+                                    edge_indices,
+                                    dims=node_feature_size,
+                                    name=self.name+"_aggregate")
+        return edge_reduce
+
+    def forward(self,
+                node_features,
+                neighbor_features,
+                edge_features,
+                edge_index):
         """Apply NNConv layer.
 
         Args:
             node_features (Layer); A 2D layer of node features of
                                    shape (num_nodes, input_channels)
-            edge_features (Layer): A 2D layer of node features of
+            neighbor_features (Layer): A 3D layer of node features of
+                                       shape (num_edges, 1, input_channels)
+            edge_features (Layer): A 2D layer of edge features of
                                    shape (num_edges, edge_features)
             edge_index (Layer): A 1D layer of node features of
                                 shape (num_edges * output_channels).
@@ -74,29 +142,13 @@ class NNConv(Module):
                      (num_nodes, self.output_channels)
         """
 
-        node_feature_size = self.num_nodes * self.output_channels
+        updated_node_fts, neighbor_vals = self.message(node_features,
+                                                       neighbor_features,
+                                                       edge_features)
+        aggregated_fts = self.aggregate(edge_features, edge_index)
 
-        updated_node_features = self.node_nn(node_features)
-
-        edge_update = self.edge_nn(edge_features)
-
-        edge_values = \
-            lbann.Reshape(edge_update,
-                          dims=str_list([node_feature_size]),
-                          name=self.name+"_edge_mat_reshape")
-
-        edge_reduce = lbann.Scatter(edge_values,
-                                    edge_index,
-                                    dims=node_feature_size,
-                                    name=self.name+"_aggregate")
-
-        updated_edge_features = \
-            lbann.Reshape(edge_reduce,
-                          dims=str_list([self.num_nodes, self.output_channels]),
-                          name=self.name+"_updated_edge_mat_reshape")
-
-        update = lbann.Sum(updated_node_features,
-                           updated_edge_features,
+        update = lbann.Sum(updated_node_fts,
+                           aggregated_fts,
                            name=self.name+"_updated_node_features")
 
         return update
