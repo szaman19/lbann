a44308906c6971820ea71761713ed1bbec5224ad
Shehtab Zaman <szaman5@binghamton.edu>
2021-06-02 20:28:52 -0400

    Added graph proto configruation

--- applications/graph/GNN/data/Synthetic/__init__.py
+++ applications/graph/GNN/data/Synthetic/__init__.py
@@ -20,3 +20,26 @@ def make_data_reader(classname,
     _reader.python.num_samples_function = num_samples
     _reader.python.sample_dims_function = sample_dims
     return reader
+
+
+def make_graph_reader(filename,
+                      num_samples,
+                      max_num_nodes,
+                      max_num_edges,
+                      num_node_features,
+                      num_edge_features,
+                      has_edge_features=True):
+    reader = lbann.reader_pb2.DataReader()
+    _reader = reader.reader.add()
+    _reader.name = 'graph'
+    _reader.role = 'train'
+    _reader.shuffle = True
+    _reader.percent_of_data_to_use = 1.0
+    _reader.data_filedir = data_dir
+    _reader.data_filename = filename
+    _reader.graph.max_node_size = max_num_nodes
+    _reader.graph.max_edge_size = max_num_edges
+    _reader.graph.num_node_features = num_node_features
+    _reader.graph.num_edge_features = num_edge_features
+    _reader.graph.has_edge_features = has_edge_features
+    return reader
