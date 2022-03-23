bd15253d90f3b51f55258093a66bc662e9e56b8a
Shehtab Zaman <szaman5@binghamton.edu>
2022-03-23 10:36:18 -0700

    Typo fixes to distconv layer

--- include/lbann/utils/distconv.hpp
+++ include/lbann/utils/distconv.hpp
@@ -53,6 +53,10 @@
 #endif // DISTCONV_HAS_P2P
 
 #include "lbann/layers/learning/distconv/distconv_layers.hpp"
+#include "lbann/layers/transform/distconv/distconv_scatter.hpp"
+#include "lbann/layers/transform/distconv/distconv_gather.hpp"
+
+
 namespace lbann {
 
 class Layer;
@@ -123,6 +127,13 @@ using BatchNormalization = ::distconv::BatchNormalization<Backend, TensorDataTyp
 using Softmax = ::distconv::Softmax<Backend>;
 using CrossEntropy = ::distconv::CrossEntropy<Backend>;
 using MeanSquaredError = ::distconv::MeanSquaredError<Backend>;
+
+#ifdef LBANN_HAS_NVSHMEM
+template <typename TensorDataType>
+using Scatter = ::distconv::Scatter<Backend, TensorDataType>;
+template <typename TensorDataType>
+using Gather = ::distconv::Gather<Backend, TensorDataType>;
+#endif // LBANN_HAS_NVSHMEM
 
 using ::distconv::get_sample_dim;
 using ::distconv::get_channel_dim;
